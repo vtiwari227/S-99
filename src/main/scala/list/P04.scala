d1ddec5f07e562c4ed7cb206fd83e30e5b6cf35b
Vipin Tiwari <vtiwari@usc.edu>
2018-08-11 23:24:22 -0500

    P04 implementation

---
+++ src/main/scala/list/P04.scala
@@ -0,0 +1,27 @@
+package list
+
+/*
+  P04 (*) find the number of elements in the list
+  Example:
+  scala > length(List(1,1,2,5,8))
+  res0: Int = 6
+ */
+class P04 {
+
+  def lengthBuiltin[A](ls: List[A]): Int = ls.length
+
+  def lengthRecursive[A](ls: List[A]): Int = ls match {
+    case Nil => 0
+    case _ :: tail => 1 + lengthRecursive(tail)
+  }
+
+  def lengthFunctional[A](ls: List[A]): Int = ls.foldLeft(0) {(c, _) => c + 1}
+
+  def lengthTailRecursive[A](ls: List[A]): Int = {
+    def lengthR(result: Int, list: List[A]): Int = list match {
+      case Nil => result
+      case _ :: tail => lengthR(result + 1, tail)
+    }
+    lengthR(0, ls)
+  }
+}
